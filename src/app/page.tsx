3668c0a9b2a3ff2e2239f8ae26c3e1dbfbb978e6
Guilherme Salkovski <guilherme.salkovski@gmail.com>
2023-09-16 17:11:04 -0300

    feat: Header - adding shadcn components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import { Button } from '@/components/ui/button'
-
 export default function Home() {
   return (
     <>
-      <h1 className="t-4 m-2 flex flex-col bg-slate-400">Hello Freello!</h1>
-      <Button />
+      <h1>Hello Freello!</h1>
     </>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,44 @@
+'use client'
+
 import { Button } from './ui/button'
+import { Grip } from 'lucide-react'
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from './ui/navigation-menu'
 
 export function Header() {
-  return <Button>Teste</Button>
+  return (
+    <header className="flex h-12 w-screen items-center justify-between bg-neutral-700 px-2">
+      <nav className="flex items-center gap-2 bg-neutral-700">
+        <Button
+          variant={'ghost'}
+          size={'icon'}
+          className="hover:bg-neutral-500"
+        >
+          <Grip className="bg-transparent text-neutral-300" />
+        </Button>
+        <Button
+          variant={'ghost'}
+          size={'lg'}
+          className="px-2 text-neutral-300 hover:bg-neutral-500"
+        >
+          Freello
+        </Button>
+        <NavigationMenu className="">
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>Área de Trabalho</NavigationMenuTrigger>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </nav>
+      <div>
+        <button> Blob</button>
+        <button> Blob2</button>
+      </div>
+    </header>
+  )
 }
