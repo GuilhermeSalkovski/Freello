cb5fa0b7be37b3517e62ecae8e82cbec3ec9ae80
luanrem <luanrem@gmail.com>
2023-09-16 15:27:31 -0300

    refactor: new eslint rules

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1 className="t-4 flex bg-slate-400">Hello Freello!</h1>
+  return <h1 className="t-4 m-2 flex flex-col bg-slate-400">Hello Freello!</h1>
 }
